a5a9bf3b752b4b7416d0e64474a0e6b46de5f4c0
Toan Ngo <45133814+ToanNgo2709@users.noreply.github.com>
2025-10-18 08:13:34 +0700

    fix issue

--- src/warehouse/warehouse.module.ts
+++ src/warehouse/warehouse.module.ts
@@ -4,9 +4,14 @@ import { WarehouseController } from './warehouse.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Warehouse } from './entities/warehouse.entity';
 import { ProductModule } from 'src/product/product.module';
+import { WarehouseGroupModule } from 'src/warehouse-group/warehouse-group.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Warehouse]), forwardRef(() => ProductModule)],
+  imports: [
+    TypeOrmModule.forFeature([Warehouse]), 
+    forwardRef(() => ProductModule),
+    forwardRef(() => WarehouseGroupModule)
+  ],
   exports: [TypeOrmModule, WarehouseService],
   controllers: [WarehouseController],
   providers: [WarehouseService]
